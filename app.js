f5143a9db34fed804c087f949b1df65b46060139
root6439 <nicolas.soaresh.98@gmail.com>
2021-07-29 18:35:16 -0300

    Puxando dados do banco

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const app = require("./config/server-config")
 
-const rotaNoticias = require("./app/routes/noticias")
+const noticias = require("./app/routes/noticias")
 const home = require("./app/routes/home")
 const formulario = require("./app/routes/formulario_inclusao_noticia")
 
 home(app)
 formulario(app)
-rotaNoticias(app)
+noticias(app)
 
 app.listen(3000, function () {
   console.log("Server up with Express");
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,8 +1,24 @@
 const methods = require("../../methods")
 
 module.exports = function (app) {
+
   app.get("/noticias", function (req, res) {
+
+    const mysql = require("mysql")
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: '123456789',
+      database: 'portal_noticias'
+    })
+
+    connection.query("select * from noticia", function (err, resp) {
+      if (err) throw err;
+      res.send(resp)
+    })
+
     console.log(methods.getRoute + req.url);
-    res.render('noticias/noticias')
   })
 }
